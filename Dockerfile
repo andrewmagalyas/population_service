24cd38422e249dfe54ca67d3081b48b005af3996
andrii <andrewmagalyas@gmail.com>
2024-12-22 22:45:41 +0100

    docker settings

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM python:3.11
 
 WORKDIR /app
 
-# Встановлення системних залежностей для psycopg2
 RUN apt-get update && apt-get install -y libpq-dev gcc
 
 COPY requirements.txt .
@@ -10,4 +9,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
+EXPOSE 8000
+ENTRYPOINT ["bash", "entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if a command is passed (either get_data or print_data)
+if [ "$1" == "get_data" ]; then
+    # Execute the code for fetching data
+    python -m app.scripts.get_data
+elif [ "$1" == "print_data" ]; then
+    # Execute the code for printing data
+    python -m app.scripts.print_data
+else
+    # Default to running the FastAPI app
+    uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+fi
